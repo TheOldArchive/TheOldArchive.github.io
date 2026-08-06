b119e96ca7988eb8bd848e2eac57882448118740
Against Interactive <zihmanisoskars05@gmail.com>
2026-08-06 18:44:18 +0300

    Delete assets/main/js/main.js

--- assets/main/js/main.js
+++
@@ -1,3 +0,0 @@
-function request(){window.location.href='https://github.com/TheOldArchive/request/issues '}
-function creator(){window.location.href='/creator '}
-function archives(){window.location.href='/archives '}
