eefa9c3b78b32de007f5b40217afff4064884155
Against Interactive <zihmanisoskars05@gmail.com>
2026-08-06 18:33:20 +0300

    Add navigation functions for request, creator, and archives

---
+++ assets/main/js/main.js
@@ -0,0 +1,3 @@
+function request(){window.location.href='https://github.com/TheOldArchive/request/issues '}
+function creator(){window.location.href='/creator '}
+function archives(){window.location.href='/archives '}
