9f221b46206dae5a5438120ab8bb6d631c81f625
Against Interactive <zihmanisoskars05@gmail.com>
2026-08-06 19:08:51 +0300

    Update creator function URL to Against Interactive

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -1,3 +1,4 @@
 function request(){window.location.href='https://github.com/TheOldArchive/request/issues '}
-function creator(){window.location.href='/creator '}
+function creator(){window.location.href='https://againstinteractive.github.io/ '}
 function archives(){window.location.href='/archives '}
+/*Against Interactive All rights reserved */
