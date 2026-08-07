079f3ee3632d918f56c8a2e3604c5eb0f6370d4c
Against Interactive <zihmanisoskars05@gmail.com>
2026-08-07 20:21:17 +0300

    Update main.js

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -2,3 +2,4 @@ function request(){window.location.href='https://github.com/TheOldArchive/reques
 function creator(){window.location.href='https://againstinteractive.github.io/ '}
 function archives(){window.location.href='/archives '}
 /*Against Interactive All rights reserved */
+console.log("Against Interactive all rights reserved https://againstinteractive.github.io")
