23102522cc1940df815c85eee60fb9dc414d5aa9
Against Interactive <zihmanisoskars05@gmail.com>
2026-08-07 20:21:44 +0300

    Fix comment formatting in main.js

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -1,5 +1,6 @@
 function request(){window.location.href='https://github.com/TheOldArchive/request/issues '}
 function creator(){window.location.href='https://againstinteractive.github.io/ '}
 function archives(){window.location.href='/archives '}
-/*Against Interactive All rights reserved */
 console.log("Against Interactive all rights reserved https://againstinteractive.github.io")
+/*Against Interactive All rights reserved */
+
